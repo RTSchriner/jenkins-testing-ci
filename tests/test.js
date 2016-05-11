5a8804a02287914456da03ce3feef64f16746654
Riley Schriner <RTSchriner@gmail.com>
2016-05-11 17:26:52 -0400

    added all files

---
+++ tests/test.js
@@ -0,0 +1,22 @@
+describe('Home Page', function() {
+  
+  describe('Page title', function () {
+    it('should be set to simple @watch', function () {
+      browser.url('http://localhost:3000');
+      expect(browser.getTitle()).to.equal('simple');
+    });
+  });
+  
+  describe('Counter Button', function () {
+    it('should be set to 0 @watch', function () {
+      var text = browser.getText('p')
+      expect(text).to.equal("You've pressed the button 0 times.");
+    });
+    it('should increment count on click @watch', function () {
+      browser.click('button');
+      var text = browser.getText('p')
+      expect(text).to.equal("You've pressed the button 1 times.");
+    });
+  });
+  
+});
